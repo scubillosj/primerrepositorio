c995bd31557a42aa3675ec113bbae8de504b1463
scubillosj <scubillosj@gmail.com>
2023-05-02 20:17:48 -0500

    Version taller

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const { request } = require('http')
 const app = express()
+const path = require('path')
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () =>{
     console.log("Escuchando al servidor en el puerto: " + port)
+})
+
+app.get('/', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, "index.html"))
 })
